80b6286ab445a1430dbf7b5831903533739fcb04
Fixs <fixs228@gmail.com>
2024-05-26 16:38:24 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "softwaretoolstest",
+  "name": "softwaretoolstest",
+  "description": "simple description",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
